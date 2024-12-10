4fb735f91481ac92c0ba3bee82111713dc60c554
Lola Corales <lolacorales@gmail.com>
2024-12-09 23:18:00 -0300

    Agregando páginas nuevas

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -4,8 +4,8 @@ from app_coder.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('inicio/', inicio),
-    path ('nuevo_libro/', nuevo_libro),
-    path ('nuevo_critico/', nuevo_critico),
-    path ('nueva_reseña/', nueva_reseña)
+    path ('inicio/', inicio, name="Inicio"),
+    path ('nuevo_libro/', nuevo_libro, name="Nuevo Libro"),
+    path ('nuevo_critico/', nuevo_critico, name="Nuevo Critico"),
+    path ('nueva_reseña/', nueva_reseña, name="Nueva Reseña")
 ]
